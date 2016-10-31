932bcd7dab108544f9f54f16a8e24d9d56fe30ac
Elizabeth Linn <elise_81@yahoo.com>
2016-10-30 20:59:52 -0700

    broken app, but the important stuff is working; wiredep task wires up bower css, js, and all app js into the html; inject task wires up app css into html and calls wirede

--- gulp.config.js
+++ gulp.config.js
@@ -3,32 +3,28 @@ module.exports = function () {
 
     var client = './src/client/';
     var clientApp = client + 'app/';
+    var temp = './.tmp/';
 
     var config = {
         alljs: [
             './src/**/*.js',
             './*.js'
         ],
-
         client: client,
-
-        temp: './.tmp/',
-
+        css: temp + 'styles.css',
+        temp: temp,
         index: client + 'index.html',
         js: [
             clientApp + '**/*.module.js',
             clientApp + '**/*.js',
             '!' + clientApp + '**/*.spec.js'
         ],
-
         less: client + 'styles/styles.less',
-
         bower: {
             json: require('./bower.json'),
             directory: './bower_components',
             ignorePath: '../..'
         }
-
     };
 
     config.getWiredepDefaultOptions = function () {
@@ -38,10 +34,7 @@ module.exports = function () {
             ignorePath: config.bower.ignorePath
         };
         return options;
-
     };
 
-
     return config;
-
 };
